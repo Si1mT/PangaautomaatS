998e4bd695bd95fcaab3d3c02a0ac559aba3ad84
Si1mT <33309403+Si1mT@users.noreply.github.com>
2017-12-07 04:48:57 +0200

    valmis

---
+++ PangaautomaatS/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PangaautomaatS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName,"Kontod");
+            var konto = new Konto();
+
+            Console.WriteLine("Sisestage number:\n1.Registreerimine\n2.Sisselogimine");
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                Console.WriteLine("Sisestage nimi");
+                konto.Name = Console.ReadLine();
+                if (!File.Exists(path + "\\" + konto.Name + ".txt"))
+                {
+                    Console.WriteLine("Sisestage PIN");
+                    konto.Pin = Console.ReadLine();
+                    File.WriteAllText(path + "\\" + konto.Name + ".txt", konto.Pin + "\n0");
+                    Console.WriteLine("Konto on registreeritud");
+                }
+                else
+                {
+                    Console.WriteLine("See nimi on võetud");
+                }
+            }
+            else if (answer == "2")
+            {
+                Console.WriteLine("Sisestage konto nimi");
+                konto.Name = Console.ReadLine();
+                if (File.Exists(path + "\\" + konto.Name + ".txt"))
+                {
+                    Console.WriteLine("Sisestage PIN");
+                    konto.Pin = Console.ReadLine();
+                    string[] lines = File.ReadAllLines(path + "\\" + konto.Name + ".txt");
+                    if (konto.Pin == lines[0])
+                    {
+                        konto.Money = int.Parse(lines[1]);
+                        Console.WriteLine("Teil on "+konto.Money+" eurot");
+                        Console.WriteLine("Kas tahate:\n    1.Raha sisestada\nvõi\n    2.Raha võtta");
+                        string option = Console.ReadLine();
+                        if (option == "1")
+                        {
+                            konto.Deposit();
+                        }
+                        else if (option == "2")
+                        {
+                            konto.Withdraw();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("See ei ole valik");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PangaautomaatS/Register.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PangaautomaatS
+{
+    class Konto
+    {
+        public string Name { get; set; }
+        public string Pin { get; set; }
+        public int Money { get; set; }
+
+        string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Kontod");
+
+        public void Deposit()
+        {
+            Console.WriteLine("Kui palju tahate arvele panna?");
+            Money=Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText(path + "\\" + Name + ".txt", Pin + "\n" + Money);
+            Console.WriteLine("Teie uus balanss: "+Money);
+        }
+        public void Withdraw()
+        {
+            Console.WriteLine("Kui palju tahate arvelt võtta?");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x <= Money)
+            {
+                Money = Money - x;
+                File.WriteAllText(path + "\\" + Name + ".txt", Pin + "\n" + Money);
+                Console.WriteLine("Teie uus balanss: "+Money);
+            }
+            else
+            {
+                Console.WriteLine("Teil pole nii palju raha");
+            }
+        }
+    }
+}
